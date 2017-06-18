47ae55d8c6e7f770432a97a4281d51fdaee5e7d5
Luke Childs <lukechilds123@gmail.com>
2017-06-18 19:00:51 +0700

    Use String#padStart

--- b64golf.js
+++ b64golf.js
@@ -5,7 +5,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 const b64Encode = str => str.split('')
   // Map character bytes to 8-bit binary values
   // unescape(encodeURIComponent(c)).split('') allows us to correctly handle multibyte characters
-  .map(c => unescape(encodeURIComponent(c)).split('').map(c => ('000000000' + c.charCodeAt(0).toString(2)).substr(-8)).join(''))
+  .map(c => unescape(encodeURIComponent(c)).split('').map(c => c.charCodeAt(0).toString(2).padStart(8, 0)).join(''))
   // Join 8-bit binary values to produce binary string
   .join('')
   // Split binary string out into 6-bit values
