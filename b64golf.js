792ec537afd1415240f56dce44645c89a0f38968
Luke Childs <lukechilds123@gmail.com>
2017-11-10 13:39:00 +0700

    Use String#padEnd

--- b64golf.js
+++ b64golf.js
@@ -12,7 +12,7 @@ const b64Encode = str => str.split('')
   .match(/.{1,6}/g)
   // Map 6-bit binary values into Base64 characters by converting binary value to alphabet index
   // Make sure to pad with zeroes if we have less than 6 bits
-  .map(b => alphabet[parseInt(parseInt(b + '0'.repeat(6 - b.length), 10), 2)])
+  .map(b => alphabet[parseInt(parseInt(b.padEnd(6, 0), 10), 2)])
   // Join the Base64 characters to produce encoded string
   // Pad with '=' if string length isn't a multiple of 4
   .reduce((s, c, i, a) => (s += c) + ((i !== a.length - 1) ? '' : '='.repeat((Math.ceil(s.length / 4) * 4) - s.length)));
