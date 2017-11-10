b7850cc664f5171a281da42de543eae86ecda2dd
Luke Childs <lukechilds123@gmail.com>
2017-11-10 13:28:53 +0700

    Reword

--- b64golf.js
+++ b64golf.js
@@ -1,4 +1,4 @@
-// Generate Base64 alphabet lookup table
+// Base64 alphabet lookup table
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
 // Transform input string into array of chars
